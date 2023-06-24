7d845fed1d5ccdd636a1ebe783bdc1f758d2c819
hoontee <hunter.chambers@gmail.com>
2023-06-24 18:27:37 -0500

    Fix compiled plugin workflow

--- Compile_Plugin.lua
+++ Compile_Plugin.lua
@@ -2,6 +2,8 @@
 local fs = require("@lune/fs")
 local roblox = require("@lune/roblox")
 
-local pluginModel = roblox.deserializeModel(fs.readFile("Lync/Plugin.source.rbxm"))
-pluginModel.Source = fs.readFile("Lync/Plugin.source.lua")
+local pluginModel = roblox.deserializeModel(fs.readFile("Lync/RobloxPluginSource/Model.rbxm"))
+local root = pluginModel[1]
+root.Source = fs.readFile("Lync/RobloxPluginSource/Plugin.lua")
+root.LuaCsv.Source = fs.readFile("Lync/RobloxPluginSource/LuaCsv.lua")
 fs.writeFile("Lync/Plugin.rbxm", roblox.serializeModel(pluginModel))
