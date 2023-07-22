edd5a2fa397311bba49559724d5a6831006bdfff
hoontee <hunter.chambers@gmail.com>
2023-07-22 13:30:24 -0500

    Delete minimatch

--- Lync/node_modules/minimatch/dist/mjs/_parse.d.ts
+++
@@ -1,7 +0,0 @@
-import { MinimatchOptions } from './index.js';
-export type MMRegExp = RegExp & {
-    _src?: string;
-    _glob?: string;
-};
-export declare const parse: (options: MinimatchOptions, pattern: string, debug: (...a: any[]) => void) => false | string;
-//# sourceMappingURL=_parse.d.ts.map
--- Lync/node_modules/minimatch/dist/mjs/extglob.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=extglob.d.ts.map
