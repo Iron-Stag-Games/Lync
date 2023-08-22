c6a88866ae952b37fed2ae831c427ebedbd2c988
hoontee <hunter.chambers@gmail.com>
2023-08-22 01:17:12 -0500

    Update Publish-main.yml

--- Compile_Lync.bat
+++ Compile_Lync.bat
@@ -1,3 +1,8 @@
 @echo off
-cd %~dp0/Lync
-pkg package.json
+cd %~dp0\Lync
+del lync-win.exe
+del lync-macos
+del lync-linux
+call pkg package.json
+copy lync-win.exe "%UserProfile%\Documents\Project Documents\Git\lync.exe"
+copy config.json "%UserProfile%\Documents\Project Documents\Git\config.json"
