246a6376740a835f4115a934eded346b6003f1eb
hoontee <hunter.chambers@gmail.com>
2023-08-23 16:51:43 -0500

    Fix permissions

--- Compile_Lync.bat
+++ Compile_Lync.bat
@@ -9,5 +9,7 @@ del "%UserProfile%\Documents\Project Documents\Git\lync.exe"
 del "%UserProfile%\Documents\Project Documents\Git\lync.exe.temp"
 del "%UserProfile%\Documents\Project Documents\Git\lync-config.json"
 call pkg package.json --no-bytecode --public-packages "*" --public
+git update-index --chmod=+x lync-macos
+git update-index --chmod=+x lync-linux
 copy lync-win.exe "%UserProfile%\Documents\Project Documents\Git\lync.exe"
 copy lync-config.json "%UserProfile%\Documents\Project Documents\Git\lync-config.json"
