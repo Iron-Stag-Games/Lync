ff579991cab153fe2c190088cbc613df83cb8114
hoontee <hunter.chambers@gmail.com>
2023-08-23 19:14:25 -0500

    Add all architectures

--- Compile_Lync.bat
+++ Compile_Lync.bat
@@ -2,14 +2,18 @@
 cd %~dp0
 lune Compile_Plugin.lua
 cd Lync
-del lync-win.exe
-del lync-macos
-del lync-linux
+del lync-win-x64.exe
+del lync-macos-x64
+del lync-macos-arm64
+del lync-linux-x64
+del lync-linux-arm64
 del "%UserProfile%\Documents\Project Documents\Git\lync.exe"
 del "%UserProfile%\Documents\Project Documents\Git\lync.exe.temp"
 del "%UserProfile%\Documents\Project Documents\Git\lync-config.json"
 call pkg package.json --no-bytecode --public-packages "*" --public
-git update-index --chmod=+x lync-macos
-git update-index --chmod=+x lync-linux
-copy lync-win.exe "%UserProfile%\Documents\Project Documents\Git\lync.exe"
+git update-index --add --chmod=+x lync-macos-x64
+git update-index --add --chmod=+x lync-macos-arm64
+git update-index --add --chmod=+x lync-linux-x64
+git update-index --add --chmod=+x lync-linux-arm64
+copy lync-win-x64.exe "%UserProfile%\Documents\Project Documents\Git\lync.exe"
 copy lync-config.json "%UserProfile%\Documents\Project Documents\Git\lync-config.json"
