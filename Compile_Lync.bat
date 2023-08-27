ef23ec5f223291ec3f8609662485db5b93c3d217
hoontee <hunter.chambers@gmail.com>
2023-08-26 20:28:35 -0500

    Update Compile_Lync.bat

--- Compile_Lync.bat
+++ Compile_Lync.bat
@@ -9,6 +9,7 @@ del lync-linux-x64
 del lync-linux-arm64
 del "%UserProfile%\Documents\Project Documents\Git\lync.exe"
 del "%UserProfile%\Documents\Project Documents\Git\lync.exe.temp"
+del "%UserProfile%\Documents\Project Documents\Git\lync-config.json"
 call pkg package.json --no-bytecode --public-packages "*" --public
 git update-index --add --chmod=+x lync-macos-x64
 git update-index --add --chmod=+x lync-macos-arm64
