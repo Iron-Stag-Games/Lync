ffb706f9cddba057e2d8b37191464a83651a3729
hoontee <hunter.chambers@gmail.com>
2023-08-27 16:41:43 -0500

    Update Compile_Lync.bat

--- Compile_Lync.bat
+++ Compile_Lync.bat
@@ -3,12 +3,12 @@ cd %~dp0
 lune Compile_Plugin.lua
 cd Lync
 del lync-win-x64.exe
-del "%UserProfile%\Documents\Project Documents\Git\lync.exe"
-del "%UserProfile%\Documents\Project Documents\Git\lync.exe.temp"
-del "%UserProfile%\Documents\Project Documents\Git\lync-config.json"
-call pkg package.json --no-bytecode --public-packages "*" --public
 del lync-macos-x64
 del lync-macos-arm64
 del lync-linux-x64
 del lync-linux-arm64
+del "%UserProfile%\Documents\Project Documents\Git\lync.exe"
+del "%UserProfile%\Documents\Project Documents\Git\lync.exe.temp"
+del "%UserProfile%\Documents\Project Documents\Git\lync-config.json"
+call pkg package.json --no-bytecode --public-packages "*" --public
 copy lync-win-x64.exe "%UserProfile%\Documents\Project Documents\Git\lync.exe"
